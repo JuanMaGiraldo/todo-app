8293f594b43315b0fe7a07ce85fd07bd297de33b
juanma <juan.giraldo@verypossible.com>
2022-09-17 10:19:41 -0500

    initial commit

---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/You have 0 todos/i);
+  expect(linkElement).toBeInTheDocument();
+});
